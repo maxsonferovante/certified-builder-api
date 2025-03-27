f92fc42d531be6b5e3729aa131843cb9a8a70da4
Maxson Almeida <maxsonferovante@gmail.com>
2025-03-27 13:08:57 -0300

    adiciona suporte a chamadas Feign para integração com TechFloripa e expõe endpoint para obter pedidos

--- src/main/java/com/maal/certifiedbuilderapi/CertifiedBuilderApiApplication.java
+++ src/main/java/com/maal/certifiedbuilderapi/CertifiedBuilderApiApplication.java
@@ -2,10 +2,11 @@ package com.maal.certifiedbuilderapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableFeignClients
 public class CertifiedBuilderApiApplication {
 
     public static void main(String[] args) {
