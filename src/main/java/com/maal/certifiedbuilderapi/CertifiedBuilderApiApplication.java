e602ed67caadcc51494ffcb1bf2273b73c03e06c
Maxson Almeida <maxsonferovante@gmail.com>
2025-03-27 11:20:19 -0300

    commit init

---
+++ src/main/java/com/maal/certifiedbuilderapi/CertifiedBuilderApiApplication.java
@@ -0,0 +1,13 @@
+package com.maal.certifiedbuilderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifiedBuilderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertifiedBuilderApiApplication.class, args);
+    }
+
+}
