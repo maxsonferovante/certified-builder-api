d0b4421b673d3d5d838ce81157d3df8b1f74c52a
Maxson Almeida <maxsonferovante@gmail.com>
2025-03-28 13:54:37 -0300

    adiciona classe S3ClientCustomer para gerar URLs assinadas e atualiza CertificateRepository com método para buscar certificados por ID de produto

---
+++ src/main/java/com/maal/certifiedbuilderapi/infrastructure/aws/s3/S3ClientCustomer.java
@@ -0,0 +1,37 @@
+package com.maal.certifiedbuilderapi.infrastructure.aws.s3;
+
+
+import com.maal.certifiedbuilderapi.infrastructure.aws.sqs.OrderEventListener;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import io.awspring.cloud.s3.S3Template;
+
+import java.net.URL;
+import java.time.Duration;
+
+
+@Component
+@AllArgsConstructor
+public class S3ClientCustomer {
+    private static final Logger logger = LoggerFactory.getLogger(S3ClientCustomer.class);
+    private final S3Properties s3Properties;
+    private final S3Template  s3Template;
+
+    public String getUrl(String key){
+        try{
+            logger.info("Getting URL for key {}", key);
+            URL response =  s3Template.createSignedGetURL(
+                    s3Properties.getBucketName(),
+                    key,
+                    Duration.ofDays(7L)
+            );
+            logger.info("Successfully retrieved URL for key {}", response.toString());
+            return response.toString();
+        } catch (Exception e){
+            logger.error(e.getMessage());
+            return null;
+        }
+    }
+}
--- src/main/java/com/maal/certifiedbuilderapi/infrastructure/repository/CertificateRepository.java
+++ src/main/java/com/maal/certifiedbuilderapi/infrastructure/repository/CertificateRepository.java
@@ -4,9 +4,11 @@ import com.maal.certifiedbuilderapi.domain.entity.CertificateEntity;
 import com.maal.certifiedbuilderapi.domain.entity.OrderEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CertificateRepository extends MongoRepository<CertificateEntity, String> {
     Optional<CertificateEntity> findByOrder(OrderEntity order);
     void deleteByOrder_Product_ProductId(Integer productId);
+    List<CertificateEntity> findByOrder_Product_ProductId(Integer productId);
 }
