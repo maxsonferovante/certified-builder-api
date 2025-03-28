de3f75f6990ed3579bfb44d33378bae5019adabb
Maxson Almeida <maxsonferovante@gmail.com>
2025-03-28 00:46:30 -0300

    adiciona método findByOrderId na interface OrderRepository para busca de ordens por ID

--- src/main/java/com/maal/certifiedbuilderapi/infrastructure/repository/OrderRespository.java
+++ src/main/java/com/maal/certifiedbuilderapi/infrastructure/repository/OrderRespository.java
@@ -4,7 +4,11 @@ import com.maal.certifiedbuilderapi.domain.entity.OrderEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface OrderRespository  extends MongoRepository<OrderEntity, String> {
+    Optional<OrderEntity> findByOrderId(Integer orderId);
+
 }
