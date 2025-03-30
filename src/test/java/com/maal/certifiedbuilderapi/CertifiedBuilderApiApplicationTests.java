da6a8539b2a62611b8d91bba5f4d39adff40edd1
Maxson Almeida <maxsonferovante@gmail.com>
2025-03-30 15:25:05 -0300

    fix disable sqs for test contextload

--- src/test/java/com/maal/certifiedbuilderapi/CertifiedBuilderApiApplicationTests.java
+++ src/test/java/com/maal/certifiedbuilderapi/CertifiedBuilderApiApplicationTests.java
@@ -1,5 +1,6 @@
 package com.maal.certifiedbuilderapi;
 
+import com.maal.certifiedbuilderapi.infrastructure.aws.sqs.OrderEventPublisher;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,8 +9,9 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(properties = {
-    "url.service.tech=http://localhost:8080", // Propriedade correta para o Feign
-    "spring.data.mongodb.uri=mongodb://myuser:mypassword@localhost:27017/admin"
+        "url.service.tech=http://localhost:8080", // Propriedade correta para o Feign
+        "spring.data.mongodb.uri=mongodb://myuser:mypassword@localhost:27017/admin",
+        "spring.cloud.aws.sqs.enabled=false" // Desativa o SQS
 })
 class CertifiedBuilderApiApplicationTests {
 
