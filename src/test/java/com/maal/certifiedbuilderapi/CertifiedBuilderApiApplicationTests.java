df1529f0e867b5e9fb6dc331e13990bd06d6abe4
Maxson Almeida <maxsonferovante@gmail.com>
2025-03-30 14:56:44 -0300

    Update gradle.yml and CertifiedBuilderApiApplicationTests.java to improve CI configuration and add SQS mocking. Cleaned up whitespace in gradle.yml and added test properties and a mock for SqsTemplate in the test class.

--- src/test/java/com/maal/certifiedbuilderapi/CertifiedBuilderApiApplicationTests.java
+++ src/test/java/com/maal/certifiedbuilderapi/CertifiedBuilderApiApplicationTests.java
@@ -1,13 +1,22 @@
 package com.maal.certifiedbuilderapi;
 
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "service.url=http://localhost:8080", // Valor fixo para o Feign
+        "spring.cloud.aws.sqs.enabled=false" // Tenta desativar o SQS
+})
 class CertifiedBuilderApiApplicationTests {
+
+    @MockBean
+    private SqsTemplate sqsTemplate; // Mock do SqsTemplate
 
     @Test
     void contextLoads() {
     }
-
 }
