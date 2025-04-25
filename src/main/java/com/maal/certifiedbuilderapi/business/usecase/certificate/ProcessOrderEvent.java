38c7944b3f647f655329d67986f17ce59fa83019
Maxson Almeida <maxsonferovante@gmail.com>
2025-04-25 14:14:34 -0300

    Enhance ProcessOrderEvent to notify certificate generation and refactor certificate handling

--- src/main/java/com/maal/certifiedbuilderapi/business/usecase/certificate/ProcessOrderEvent.java
+++ src/main/java/com/maal/certifiedbuilderapi/business/usecase/certificate/ProcessOrderEvent.java
@@ -1,9 +1,12 @@
 package com.maal.certifiedbuilderapi.business.usecase.certificate;
 
+import com.maal.certifiedbuilderapi.business.dto.RecoverCertificatesResponse;
+import com.maal.certifiedbuilderapi.business.mapper.CertificanteMapper;
 import com.maal.certifiedbuilderapi.domain.event.OrderEvent;
 import com.maal.certifiedbuilderapi.domain.entity.CertificateEntity;
 import com.maal.certifiedbuilderapi.domain.entity.OrderEntity;
 import com.maal.certifiedbuilderapi.infrastructure.aws.s3.S3ClientCustomer;
+import com.maal.certifiedbuilderapi.infrastructure.client.TechFloripa;
 import com.maal.certifiedbuilderapi.infrastructure.repository.CertificateRepository;
 import com.maal.certifiedbuilderapi.infrastructure.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 /**
  * Use case responsible for processing order events and managing certificates.
@@ -26,6 +30,8 @@ public class ProcessOrderEvent {
     private final OrderRepository orderRepository;
     private final CertificateRepository certificateRepository;
     private final S3ClientCustomer s3ClientCustomer;
+    private final CertificanteMapper certificanteMapper;
+    private final TechFloripa techFloripa;
 
     /**
      * Processes an order event and updates or creates the associated certificate.
@@ -47,19 +53,19 @@ public class ProcessOrderEvent {
      * @param event The order event
      */
     private void processCertificate(OrderEntity order, OrderEvent event) {
-        certificateRepository.findByOrder(order)
-                .map(existingCertificate -> updateExistingCertificate(existingCertificate, event))
-                .orElseGet(() -> createNewCertificate(order, event));
+        Optional<CertificateEntity> existingCertificate = certificateRepository.findByOrder(order);
+        existingCertificate.ifPresentOrElse(
+                certificateEntity -> updateExistingCertificate(certificateEntity, event),
+                () -> createNewCertificate(order, event));
     }
 
     /**
      * Updates an existing certificate based on the event.
      *
      * @param certificate The existing certificate
-     * @param event The order event
-     * @return The updated certificate
+     * @param event       The order event
      */
-    private CertificateEntity updateExistingCertificate(CertificateEntity certificate, OrderEvent event) {
+    private void updateExistingCertificate(CertificateEntity certificate, OrderEvent event) {
         Boolean eventSuccess = event.getSuccess();
         if (eventSuccess != null && eventSuccess) {
             s3ClientCustomer.deleteCertificate(certificate.getCertificateKey());
@@ -69,7 +75,8 @@ public class ProcessOrderEvent {
             certificate.setSuccess(true);
             certificate.setGeneretedDate(LocalDateTime.now());
         }
-        return certificateRepository.save(certificate);
+        certificateRepository.save(certificate);
+        notifiesCertificateGeneration(certificate);
     }
 
     /**
@@ -77,19 +84,27 @@ public class ProcessOrderEvent {
      *
      * @param order The order entity
      * @param event The order event
-     * @return The new certificate
      */
-    private CertificateEntity createNewCertificate(OrderEntity order, OrderEvent event) {
+    private void createNewCertificate(OrderEntity order, OrderEvent event) {
         CertificateEntity certificate = new CertificateEntity();
         certificate.setOrder(order);
-        certificate.setSuccess(event.getSuccess() != null ? event.getSuccess() : false);
+        certificate.setSuccess(event.getSuccess() != null && event.getSuccess());
         
         if (Boolean.TRUE.equals(event.getSuccess())) {
             certificate.setCertificateKey(event.getCertificateKey());
             certificate.setCertificateUrl(s3ClientCustomer.getUrl(event.getCertificateKey()));
             certificate.setGeneretedDate(LocalDateTime.now());
         }
-        
-        return certificateRepository.save(certificate);
+
+        certificateRepository.save(certificate);
+        notifiesCertificateGeneration(certificate);
+    }
+
+    private void notifiesCertificateGeneration(CertificateEntity certificate) {
+        RecoverCertificatesResponse recoverCertificatesResponse = certificanteMapper.certificateEntityToResponse(
+                certificate
+        );
+        techFloripa.notifiesCertificateGeneration(recoverCertificatesResponse);
+
     }
 } 
